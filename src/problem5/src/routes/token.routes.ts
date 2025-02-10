8ddd645b29f98ed5347968baa40264b74ee03c35
Aiden Khoa <aiden.khoa@logtechub.com>
2025-02-10 11:19:09 +0700

    feat: Scaffolding Frontend

--- src/problem5/src/routes/token.routes.ts
+++ src/problem5/src/routes/token.routes.ts
@@ -10,8 +10,28 @@ const router = Router()
  * Method: GET
  */
 router.get('', errorHandler(tokenControllers.getTokens))
+/**
+ * Description: Add Token
+ * Path: /:tweet_id
+ * Method: GET
+ * Body: {
+    currency?: string;
+    price?: number;
+    image?: string;
+  } 
+ */
 router.post('/', tokenControllers.addTokenValidator, errorHandler(tokenControllers.addToken))
+/**
+ * Description: Update Token
+ * Path: /:tokenId
+ * Method: PATCH
+ */
 router.patch('/:tokenId', tokenControllers.tokenIdParamValidator, errorHandler(tokenControllers.updateToken))
+/**
+ * Description: Delete Token
+ * Path: /:tokenId
+ * Method: DELETE
+ */
 router.delete('/:tokenId', tokenControllers.tokenIdParamValidator, errorHandler(tokenControllers.deleteToken))
 
 const tokenRouter = router
